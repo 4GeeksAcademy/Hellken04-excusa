5bef338d5eaef20a23d7c69f30266931b64add51
Hellken04 <elkin_rondon@hotmail.com>
2025-04-12 03:27:31 +0000

    Hellken04-excusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomArray (anyArray){
+  let maxIndice = anyArray.length;
+  return anyArray[getRandomInt(maxIndice)];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excuse = randomArray(who) +" "+ randomArray(action) + " " + randomArray(what)+" "+randomArray(when);
+  document.getElementById("excuse").innerText = excuse;
+}
