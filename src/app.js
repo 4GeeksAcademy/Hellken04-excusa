02ce9a931027d3793081292bf44032ecf3373f1e
Hellken04 <elkin_rondon@hotmail.com>
2025-04-18 19:42:09 +0000

    Se quitaron las variables globales y pasaron todos los nombres a Inglés

--- src/app.js
+++ src/app.js
@@ -4,22 +4,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(maxNumber) {
+  return Math.floor(Math.random() * maxNumber);
 }
 
 function randomArray (anyArray){
-  let maxIndice = anyArray.length;
-  return anyArray[getRandomInt(maxIndice)];
+  let maxIndex = anyArray.length;
+  return anyArray[getRandomInt(maxIndex)];
 }
 
 window.onload = function() {
   //write your code here
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
   let excuse = randomArray(who) +" "+ randomArray(action) + " " + randomArray(what)+" "+randomArray(when);
   document.getElementById("excuse").innerText = excuse;
 }
